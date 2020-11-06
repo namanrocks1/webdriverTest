7a2409b9d7975b29b8750bc20d2182b6b86d1575
choki porwal <naman>
2020-11-06 20:10:08 +0530

    my first commit

---
+++ src/main/java/testgit/testgitserver/App.java
@@ -0,0 +1,13 @@
+package testgit.testgitserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testgit/testgitserver/LoginTest.java
@@ -0,0 +1,34 @@
+package testgit.testgitserver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32 (4)\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://portfolio.rediff.com/portfolio-login");
+	}
+	
+	@Test
+	public void doLoginTest() {
+		
+		driver.findElement(By.id("useremail")).sendKeys("testing@test.com");
+		driver.findElement(By.id("userpass")).sendKeys("test123");
+		
+	}
+	
+	
+	@AfterSuite
+	public void teardown() {
+		driver.quit();
+	}
+}
